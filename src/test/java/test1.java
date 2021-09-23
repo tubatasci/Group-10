a813f6575ee29269fabcfdfa32226e5e1d0d5065
tubatasci <tuba.avci16@gmail.com>
2021-09-23 19:13:07 +0200

    third commit

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
